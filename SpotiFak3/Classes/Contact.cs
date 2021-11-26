4fc85461c25e1782bd065dcbc203427ac3ec5fb7
Caitlin Bond <401307@vistacollege.nl>
2021-11-26 12:10:16 +0100

    Add project files.

---
+++ SpotiFak3/Classes/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace SpotiFak3.Classes
+{
+    class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        //AutoIncrement zorgt ervoor dat ID altijd uniek is.
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ SpotiFak3/SignUp.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SpotiFak3.Classes;
+using SQLite;
+
+
+namespace SpotiFak3
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            //Connection to class
+            Contact contact = new Contact()
+            {
+                Password = txtPassword.Text,
+                Email = txtEmail.Text,
+            };
+
+            //Connection to database
+            SQLiteConnection connection = new SQLiteConnection(App.strDatabasePath);
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            //Close current window
+            Close();
+        }
+    }
+}
